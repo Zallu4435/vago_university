a0ef4cea8bb572a318d8976c1c140d72a0d2ad34
Zallu-Safe <nazalzallu123@gmail.com>
2025-06-28 21:36:12 +0530

    ip address added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,7 +14,7 @@ const httpServer = createServer(app);
 // Enable CORS
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["http://localhost:5173", "http://localhost:3000", "http://10.0.14.4:5173", "http://10.0.14.4:3000", "http://10.0.14.4"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],

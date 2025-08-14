bac66090cd6c4d86e6888bd5e07b39fd86d907c9
Muhammed Nazal K <muhammednazal28@gmail.com>
2025-08-14 06:04:48 +0000

    Isolate chat feature: remove unrelated code, keep only relevant backend and frontend 4-layer files.

--- backend/src/application/site-management/repositories/IUserSiteSectionRepository.ts
+++
@@ -1,6 +0,0 @@
-import { ISiteSectionDocument } from '../../../domain/site-management/entities/SiteSectionTypes';
-
-export interface IUserSiteSectionRepository {
-  findSectionsRaw(query, skip: number, limit: number): Promise<ISiteSectionDocument[]>;
-  countSectionsRaw(query): Promise<number>;
-} 

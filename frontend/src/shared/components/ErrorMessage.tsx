d7a11c737cd1e088a8f376700037ec5ace1d89dd
Muhammed Nazal K <muhammednazal28@gmail.com>
2025-08-15 08:54:24 +0000

    Remove unrelated code from feature/student

--- frontend/src/shared/components/ErrorMessage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ErrorMessage: React.FC<{ message: string }> = ({ message }) => (
-  <div className="flex items-center justify-center min-h-screen">
-    <div className="text-red-500">Error: {message}</div>
-  </div>
-);
-
-export default ErrorMessage; 

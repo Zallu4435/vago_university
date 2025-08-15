96175e33f3eb43bc209f0bfb1a39f8565e7a0405
Muhammed Nazal K <muhammednazal28@gmail.com>
2025-08-15 08:53:07 +0000

    Remove unrelated code from feature/profile

--- backend/src/domain/profile/enums/ProfileErrorType.ts
+++
@@ -1,9 +0,0 @@
-export enum ProfileErrorType {
-    MissingRequiredFields = "All required fields must be provided!",
-    UserNotFound = "User not found!",
-    EmailAlreadyInUse = "Email already in use!",
-    IncorrectCurrentPassword = "Current password is incorrect!",
-    PasswordsDoNotMatch = "New password and confirmation do not match!",
-    ProfilePictureRequired = "Profile picture file is required!",
-    UserNotAuthenticated = "User not authenticated!",
-  }
